7d0328948a4a31736643ebc7bf8b552e6e2289e7
KietlinskiA <adrian.kietlinski@interia.pl>
2023-08-06 22:10:50 +0200

    Initial commit

---
+++ src/main/java/pl/kietlinski/kursspringboot2/KursSpringBoot2Application.java
@@ -0,0 +1,13 @@
+package pl.kietlinski.kursspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KursSpringBoot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KursSpringBoot2Application.class, args);
+    }
+
+}
